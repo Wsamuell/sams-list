461ab8378b36b797548383fea85a13b2bd80f61c
Samuel Wemimo <wemsamuel@gmail.com>
2022-08-28 10:22:37 -0700

    db setup for users and owed amount

---
+++ server/database.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE samslist;
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  username CHAR(15) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(30) NOT NULL,
+  created_on TIMESTAMPTZ
+);
+
+CREATE TABLE money_owed(
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  lender_name VARCHAR(255) NOT NULL,
+  amount_owed INT,
+  interest_rate DECIMAL,
+  due_date DATE,
+  created_on TIMESTAMPTZ,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
